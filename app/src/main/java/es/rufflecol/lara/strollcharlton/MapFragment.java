0418105c1d89b566e60c65e798c5f55fe4dc7165
lararufflecoles <lararuffle@lararuffle.co.uk>
2015-12-29 17:44:14 +0000

    Changed map centering from user location to specific LatLng

--- app/src/main/java/es/rufflecol/lara/strollcharlton/MapFragment.java
+++ app/src/main/java/es/rufflecol/lara/strollcharlton/MapFragment.java
@@ -5,6 +5,7 @@ import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -25,15 +26,56 @@ import java.util.List;
 import java.util.Map;
 
 public class MapFragment extends SupportMapFragment implements
-        OnMapReadyCallback, GoogleMap.OnMyLocationChangeListener, GoogleMap.OnInfoWindowClickListener /* interfaces */ {
+        OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener /* interfaces */ {
 
-    private boolean flag = true;
     private Map<String, DetailData> markerIdToDetailDataMap = new HashMap<>(); // HashMap added so we can combine DetailData and Marker
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getMapAsync(this);
+    }
+
+    public void setMapType(int type) { /** Method called when user taps MapActivity.java toolbar action button **/
+        getMap().setMapType(type);
+    }
+
+    private void initialiseMap(GoogleMap map) { /** Method putting all the tasks together that need to be run when the map is initialised **/
+        map.setMyLocationEnabled(true);
+        map.getUiSettings().setZoomControlsEnabled(true); /** Adds the + - options to the screen **/
+        map.setOnInfoWindowClickListener(this);
+    }
+    
+    private void setLocationToCharlton(GoogleMap map) {
+        LatLng charlton = new LatLng(51.481414, 0.035837);
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(charlton, 14f);
+        map.animateCamera(cameraUpdate);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        initialiseMap(map);
+        setLocationToCharlton(map);
+
+        List<DetailData> dataList = readDataFromFileAndParse();
+        for (DetailData item : dataList) {
+            MarkerOptions markerOptions = new MarkerOptions()
+                    .position(new LatLng(item.getLatitude(), item.getLongitude()))
+                    .title(item.getTitle())
+                    .snippet(item.getSnippet());
+            Marker marker = map.addMarker(markerOptions); // 'Marker marker =' now assigns 'map.addMarker(markerOptions)' to itself - a local variable
+            String markerId = marker.getId(); // We can now reference marker and get its ID... and assign that to markedId
+            markerIdToDetailDataMap.put(markerId, item); // Therefore, markerId and item can now be put into the HashMap markerIdToDetailDataMap
+        }
+    }
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+        Intent detailActivity = new Intent(getActivity(), DetailActivity.class);
+        String markerId = marker.getId(); // Getting the id of the marker clicked
+        DetailData item = markerIdToDetailDataMap.get(markerId); // Getting the DetailData item from the HashMap using the marker id as the key
+        detailActivity.putExtra(DetailActivity.PUT_EXTRA_DETAIL_DATA_ITEM, item); //
+        startActivity(detailActivity);
     }
 
     private List<DetailData> readDataFromFileAndParse() {
@@ -68,53 +110,5 @@ public class MapFragment extends SupportMapFragment implements
             Log.e("Login activity", "Cannot read file: " + exception.toString());
         }
         return returnValue;
-    }
-
-    private void initialiseMap() { /** Method I created, not one called from any of the classes or interfaces added above **/
-        getMap().setMyLocationEnabled(true);
-        getMap().getUiSettings().setZoomControlsEnabled(true);
-        getMap().setOnMyLocationChangeListener(this);
-        getMap().setOnInfoWindowClickListener(this);
-    }
-
-    public void setMapType(int type) { /** Method I created, not one called from any of the classes or interfaces added above **/
-        getMap().setMapType(type);
-    }
-
-    @Override
-    public void onMapReady(GoogleMap map) {
-        initialiseMap();
-
-        List<DetailData> dataList = readDataFromFileAndParse();
-        for (DetailData item : dataList) {
-            MarkerOptions markerOptions = new MarkerOptions()
-                    .position(new LatLng(item.getLatitude(), item.getLongitude()))
-                    .title(item.getTitle())
-                    .snippet(item.getSnippet());
-            Marker marker = map.addMarker(markerOptions); // 'Marker marker =' now assigns 'map.addMarker(markerOptions)' to itself - a local variable
-            String markerId = marker.getId(); // We can now reference marker and get its ID... and assign that to markedId
-            markerIdToDetailDataMap.put(markerId, item); // Therefore, markerId and item can now be put into the HashMap markerIdToDetailDataMap
-        }
-    }
-
-    @Override
-    public void onMyLocationChange(Location location) {
-        if (flag) {
-            CameraPosition position = CameraPosition.builder()
-                    .target(new LatLng(location.getLatitude(), location.getLongitude()))
-                    .zoom(16f)
-                    .build();
-            getMap().animateCamera(CameraUpdateFactory.newCameraPosition(position), null);
-            flag = false;
-        }
-    }
-
-    @Override
-    public void onInfoWindowClick(Marker marker) {
-        Intent detailActivity = new Intent(getActivity(), DetailActivity.class);
-        String markerId = marker.getId(); // Getting the id of the marker clicked
-        DetailData item = markerIdToDetailDataMap.get(markerId); // Getting the DetailData item from the HashMap using the marker id as the key
-        detailActivity.putExtra(DetailActivity.PUT_EXTRA_DETAIL_DATA_ITEM, item); //
-        startActivity(detailActivity);
     }
 }
